3e3523459ec621755c04ae45bb10600ab002f383
slivcaigor <slivcaigor@gmail.com>
2023-02-13 16:14:40 +0100

    Models

---
+++ app/Models/Person.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        
+        'name',
+        'mail',
+    ];
+
+    public function personDetail() {
+
+        return $this -> hasOne(PersonDetail :: class);
+    }
+    public function posts() {
+
+        return $this -> hasMany(Post :: class);
+    }
+}
---
+++ app/Models/PersonDetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PersonDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        
+        'phone',
+        'address',
+        'avatar',
+    ];
+
+    public function person() {
+
+        return $this -> belongsTo(Person :: class);
+    }
+}
